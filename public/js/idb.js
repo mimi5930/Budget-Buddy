fb55c4da0e55c30e7ab74dcf9c51fc7a6fba379a
Mike <mikej.miller440@gmail.com>
2022-04-09 13:31:19 -0500

    offline changes are now saved to the server when online again

--- public/js/idb.js
+++ public/js/idb.js
@@ -20,8 +20,7 @@ request.onsuccess = e => {
 
   // check if app's online
   if (navigator.onLine) {
-    // insert code here
-    // uploadTransaction()
+    uploadTransaction();
   }
 };
 
@@ -33,7 +32,7 @@ request.onerror = e => {
 // if attempt to submit without connection
 const saveRecord = record => {
   // open transaction
-  const transaction = db.transaction(['new_transaction', 'readwrite']);
+  const transaction = db.transaction(['new_transaction'], 'readwrite');
 
   // access transaction's objectStore
   const transactionObjectStore = transaction.objectStore('new_transaction');
@@ -41,3 +40,51 @@ const saveRecord = record => {
   // add record
   transactionObjectStore.add(record);
 };
+
+const uploadTransaction = () => {
+  // open transaction
+  const transaction = db.transaction(['new_transaction'], 'readwrite');
+
+  // access transaction's objectStore
+  const transactionObjectStore = transaction.objectStore('new_transaction');
+
+  // get records from store and set to a variable. Async!
+  const getAll = transactionObjectStore.getAll();
+
+  // if successful
+  getAll.onsuccess = () => {
+    // if there's data, send to api server
+    if (getAll.result.length > 0) {
+      // POST
+      fetch('/api/transaction', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(response => response.json())
+        .then(serverResponse => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+
+          // clear the store
+          const transaction = db.transaction(['new_transaction'], 'readwrite');
+          const transactionObjectStore =
+            transaction.objectStore('new_transaction');
+          transactionObjectStore.clear();
+
+          alert('All deposits and withdrawals have been submitted!');
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  };
+};
+
+// when app comes back online
+
+window.addEventListener('online', uploadTransaction);
