6a03522a922deb9873b2c825ffbb4ee739b5d763
Mike <mikej.miller440@gmail.com>
2022-04-09 14:49:53 -0500

    added install and activate event listeners for sw

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,10 @@
+// cache names
+const APP_PREFIX = 'BudgetBuddy-';
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+
+// paths to files
 const FILES_TO_CACHE = [
-  './',
   './index.html',
   './css/styles.css',
   './js/idb.js',
@@ -14,7 +19,38 @@ const FILES_TO_CACHE = [
   './icons/icon-512x512.png'
 ];
 
-self.addEventListener('install', event => {
-  //
-  //
+// run if page installed service-worker
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('installing cache : ' + CACHE_NAME);
+
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    // find only this cache
+    caches.keys().then(keyList => {
+      let cacheKeepList = keyList.filter(key => {
+        return key.indexOf(APP_PREFIX);
+      });
+
+      // add current cache
+      cacheKeepList.push(CACHE_NAME);
+
+      // delete old versions
+      return Promise.all(
+        keyList.map((key, i) => {
+          if (cacheKeepList.indexOf(key) === -1) {
+            console.log('deleting cache : ' + keyList[i]);
+
+            return caches.delete(keyList[i]);
+          }
+        })
+      );
+    })
+  );
 });
